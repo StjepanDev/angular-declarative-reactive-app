cd8054c507b136f6f9d33e39b35c411692c34d0c
StjepanDev <stjepansuhic@yahoo.com>
2023-04-08 23:35:12 +0200

    action-data stream combo

--- src/app/pages/declarative-posts/declarative-posts.component.ts
+++ src/app/pages/declarative-posts/declarative-posts.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map } from 'rxjs';
+import { DeclarativeCategoryService } from 'src/app/services/declarativecategory.service';
 import { DeclarativePostService } from 'src/app/services/declarativepostservice';
 
 @Component({
@@ -8,7 +10,29 @@ import { DeclarativePostService } from 'src/app/services/declarativepostservice'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeclarativePostsComponent {
-  constructor(private postService: DeclarativePostService) {}
+  constructor(
+    private postService: DeclarativePostService,
+    private categoryService: DeclarativeCategoryService
+  ) {}
 
   posts$ = this.postService.postsWithCategory$;
+  categories$ = this.categoryService.categories$;
+  selectedCategoryId = '';
+
+  filteredPosts$ = this.posts$.pipe(
+    map((posts) => {
+      return posts.filter((post) =>
+        // zapamti ovaj filter
+        this.selectedCategoryId
+          ? post.categoryId === this.selectedCategoryId
+          : true
+      );
+    })
+  );
+
+  onCategoryChange(event: Event) {
+    let selectedCategoryId = (event.target as HTMLSelectElement).value;
+    console.log(selectedCategoryId);
+    this.selectedCategoryId = selectedCategoryId;
+  }
 }
